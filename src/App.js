647a978a21c8e13e4e54430b2fba2c3544156e4d
Kartikey Singh <kartikeysingh16@gmail.com>
2021-08-16 13:19:40 +0530

    [React] [Learning] Adding components properties

--- src/App.js
+++ src/App.js
@@ -2,36 +2,39 @@
 import './App.css';
 
 
-function Header() {
+function Header(props) {
+  console.log(props)
   return (
     <header>
-      <h1>Eve's Kitchen</h1>
+      <h1>{props.name}'s Kitchen</h1>
     </header>
   );
 }
 
-function Main() {
+function Main(props) {
+  console.log(props)
   return (
     <section>
-      <p>We serve the most delicious food around.</p>
+      <p>We serve the most {props.adjective} food around.</p>
     </section>
   );
 }
 
-function Footer() {
+function Footer(p) {
+  console.log(p)
   return (
     <footer>
-      <p>It's true.</p>
+      <p>Copyright {p.year}</p>
     </footer>
-  )
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Header name="Kartikey" />
+      <Main adjective="amazing" />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
