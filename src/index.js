09203176eea6094fae0592902c92bde55829a7ce
Kartikey Singh <kartikeysingh16@gmail.com>
2021-08-16 11:33:28 +0530

    [React] [Learning] Created h1 element with style color blue

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement("h1", { style: { color: "blue" } }, "Heyy Everyone!"),
   document.getElementById('root')
 );
 
